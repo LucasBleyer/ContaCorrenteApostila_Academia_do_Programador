73f57126b8172a2ac00d3e3f00d1d0d2925d6d21
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-31 15:39:32 -0300

    Add project files.

---
+++ ContaCorrenteApostila.ConsoleApp/ModuloConta/Conta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrenteApostila.ConsoleApp.ModuloConta
+{
+    internal class Conta
+    {
+        private double saldo;
+
+        public double Saldo
+        {
+            get { return saldo; }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+    }
+}
